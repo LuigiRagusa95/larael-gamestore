9f547359009892acde7a412f031e81a7cf33aa8a
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2022-02-07 10:32:45 +0100

    creazione tabella pivot

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /* collegamento con i tag */
+    public function tags() {
+        return $this->belongsToMany('App\Tag');
+    }
 }
