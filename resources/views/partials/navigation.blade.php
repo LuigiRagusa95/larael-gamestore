a06234471ad25002bae0b07c58ca4e68800d0e6e
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2022-02-07 10:42:35 +0100

    creazione delle prime crud

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -8,7 +8,15 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('products.index') }}">Products</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('products.create') }}">Create new product</a>
                 </li>
             </ul>
         </div>
